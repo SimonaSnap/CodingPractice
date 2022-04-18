b8ef7234754982f071b28f1ff08a85244aa62be9
Simona <simona.snapk@gmail.com>
2022-04-18 00:05:48 -0700

    working on fizz buzz

--- week5/01-fizz-buzz/Unsolved/fizz-buzz.js
+++ week5/01-fizz-buzz/Unsolved/fizz-buzz.js
@@ -4,4 +4,20 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function (arr)
+{
+    for (let i = arr[0]; i < arr.length; i++)
+    {
+        if (i % 5 === 0 && i % 3 === 0)
+        {
+            arr[i] = "FizzBuzz"
+        } else if (i % 5 === 0)
+        {
+            arr[i] = "Buzz";
+        } else if (i % 3 === 0)
+        {
+            arr[i] = "Fizz";
+        }
+        console.log(arr);
+    }
+};
